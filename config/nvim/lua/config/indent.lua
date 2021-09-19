405584900fe04d4687df92d3e67b05515f692f09
Michael Walsh <Michael.Walsh@canterbury.ac.nz>
2021-09-19 17:37:54 +1200

    Switch terminal to fira code for ligatures etc..
    Fix indentline to be 1 continuous line

--- config/nvim/lua/config/indent.lua
+++ config/nvim/lua/config/indent.lua
@@ -1,5 +1,4 @@
 require("indent_blankline").setup({
-	char = "|",
 	show_current_context = true,
 	buftype_exclude = { "terminal", "dashboard", "startify", "alpha" },
 	filetype_exclude = { "dashboard", "alpha", "CHADTree" },
