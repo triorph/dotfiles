16c0d987e39f052c226929e3fced592bf1d354eb
Michael Walsh <Michael.Walsh@canterbury.ac.nz>
2021-09-23 19:08:13 +1200

    Add typescript LSP support (surprisingly easy)

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -40,7 +40,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "pyright" }
+local servers = { "pyright", "tsserver" }
 for _, lsp in ipairs(servers) do
 	nvim_lsp[lsp].setup(coq.lsp_ensure_capabilities({
 		on_attach = on_attach,
