0182e3cd3190982613286b913b6c7ec6969892c8
Michael Walsh <Michael.Walsh@canterbury.ac.nz>
2021-11-26 08:23:27 +1300

    Fix telescope find files to actually let me see the files

--- config/nvim/lua/config/trouble.lua
+++ config/nvim/lua/config/trouble.lua
@@ -27,6 +27,7 @@ local telescope = require("telescope")
 
 telescope.setup({
 	defaults = {
+		path_display = { "truncate" },
 		mappings = {
 			i = { ["<c-t>"] = trouble.open_with_trouble },
 			n = { ["<c-t>"] = trouble.open_with_trouble },
