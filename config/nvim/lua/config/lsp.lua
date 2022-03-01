4c630a2a79bf03bf44f09f32451b14e1f9417330
Michael Walsh <Michael.Walsh@canterbury.ac.nz>
2022-03-02 10:35:06 +1300

    Don't show virtual text for language server hints

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -38,6 +38,12 @@ local on_attach = function(client, bufnr)
 	buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 end
 
+vim.diagnostic.config({
+	virtual_text = {
+		severity = { min = vim.diagnostic.severity.WARN },
+	},
+})
+
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local servers = { "pyright" }
