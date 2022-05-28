948bb6bb7b432ecc528442e93fd0b8c97bd0e37e
Michael Walsh <Michael.Walsh@canterbury.ac.nz>
2022-05-29 10:54:07 +1200

    Make a function for toggling the windows, and allow changing chrome too

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,25 +1,30 @@
-hs.hotkey.bind({ "ctrl" }, "`", function()
-	local app = hs.application.get("kitty")
-	print(app:name())
-	local screen = hs.screen.mainScreen()
-	if app then
-		if not app:mainWindow() then
-			app = hs.application.open("kitty", 2.0, true)
-		elseif app:isFrontmost() then
-			app:hide()
-		else
-			if not (app:mainWindow().screen == screen) then
-				app:mainWindow():moveToScreen(screen)
-			end
+function toggle_window(opts, key, name)
+	hs.hotkey.bind(opts, key, function()
+		local app = hs.application.get(name)
+		local screen = hs.screen.mainScreen()
+		if app then
+			if not app:mainWindow() then
+				app = hs.application.open(name, 2.0, true)
+			elseif app:isFrontmost() then
+				app:hide()
+			else
+				if not (app:mainWindow().screen == screen) then
+					app:mainWindow():moveToScreen(screen)
+				end
 
-			app:activate()
+				app:activate()
+			end
+		else
+			app = hs.application.open(name, 2.0, true)
 		end
-	else
-		app = hs.application.open("kitty", 2.0, true)
-	end
-	app:mainWindow():moveToUnit("[100,100,0,0]")
-	-- app:mainWindow().setShadows(false)
-end)
+		-- app:mainWindow():moveToUnit("[100,100,0,0]")
+		-- app:mainWindow().setShadows(false)
+	end)
+end
+
+toggle_window({ "ctrl" }, "`", "kitty")
+-- toggle_window({"ctrl"}, "tab", "emacs")
+toggle_window({ "ctrl" }, "tab", "Google Chrome")
 
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
