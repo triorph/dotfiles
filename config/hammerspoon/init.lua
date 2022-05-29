c295734b9ffb6c20905ffd1dc0a599ff5d3adeb9
Michael Walsh <Michael.Walsh@canterbury.ac.nz>
2022-05-29 21:26:46 +1200

    Fix the window sizes for different apps better

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,4 +1,7 @@
-function toggle_window(opts, key, name)
+function toggle_window(opts, key, name, unit)
+	if unit == nil then
+		unit = {x=0.02, y=0.02, w=0.96, h=0.96}
+	end
 	hs.hotkey.bind(opts, key, function()
 		local app = hs.application.get(name)
 		local screen = hs.screen.mainScreen()
@@ -17,7 +20,7 @@ function toggle_window(opts, key, name)
 		else
 			app = hs.application.open(name, 2.0, true)
 		end
-		app:mainWindow():moveToUnit("[100,100,0,0]")
+		app:mainWindow():moveToUnit(unit)
 		app:mainWindow().setShadows(false)
 	end)
 end
@@ -25,7 +28,7 @@ end
 toggle_window({ "ctrl" }, "`", "kitty")
 -- toggle_window({"ctrl"}, "tab", "emacs")
 toggle_window({ "ctrl" }, "tab", "Google Chrome")
-toggle_window({ "ctrl", "alt"}, "s", "Spotify")
+toggle_window({ "ctrl", "alt"}, "s", "Spotify", { x=0.1, y=0.1, w=.8, h=.8 })
 
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
