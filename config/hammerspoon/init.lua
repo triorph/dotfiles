525e8817aa4b05e06e129b490796adad3ce3d4a2
Michael Walsh <mwalsh2@atlassian.com>
2022-07-21 10:13:38 +1200

    Add in a function to enlarge the space a window takes up on the screen if needed

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,15 +1,24 @@
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
-
 local move_screen = function()
-    print("Moving window")
-    local window = hs.window.frontmostWindow()
-    local next = window:screen():next()
-    window:moveToScreen(next)
+	print("Moving window")
+	local window = hs.window.frontmostWindow()
+	local next = window:screen():next()
+	window:moveToScreen(next)
 end
 
-hs.hotkey.bind({"ctrl", "alt"}, "m", move_screen)
+local embiggen_window = function(unit)
+	print("Embiggening this window")
+	local window = hs.window.frontmostWindow()
+	if unit == nil then
+		unit = { x = 0.02, y = 0.02, w = 0.96, h = 0.96 }
+	end
+	window:moveToUnit(unit)
+end
+
+hs.hotkey.bind({ "ctrl", "alt" }, "m", move_screen)
+hs.hotkey.bind({ "ctrl", "alt" }, "b", embiggen_window)
 
 require("toggle_window")
 require("newwp")
