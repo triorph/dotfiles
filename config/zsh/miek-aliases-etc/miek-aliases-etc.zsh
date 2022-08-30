8c241c130a218b666a34142a3b89bc701a8950e7
Michael Walsh <mwalsh2@atlassian.com>
2022-08-30 13:53:47 +1200

    add in a git checkout fzf script

--- config/zsh/miek-aliases-etc/miek-aliases-etc.zsh
+++ config/zsh/miek-aliases-etc/miek-aliases-etc.zsh
@@ -1,6 +1,30 @@
 
 export LESS="--quit-if-one-screen --RAW-CONTROL-CHARS --no-init"
 
+# from https://github.com/beauwilliams/awesome-fzf/blob/master/awesome-fzf.zsh
+# Checkout to existing branch or else create new branch. gco <branch-name>.
+# Falls back to fuzzy branch selector list powered by fzf if no args.
+fzf-checkout(){
+    if git rev-parse --git-dir > /dev/null 2>&1; then
+        if [[ "$#" -eq 0 ]]; then
+            local branches branch
+            branches=$(git branch -a) &&
+            branch=$(echo "$branches" |
+            fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+            git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+        elif [ `git rev-parse --verify --quiet $*` ] || \
+             [ `git branch --remotes | grep  --extended-regexp "^[[:space:]]+origin/${*}$"` ]; then
+            echo "Checking out to existing branch"
+            git checkout "$*"
+        else
+            echo "Creating new branch"
+            git checkout -b "$*"
+        fi
+    else
+        echo "Can't check out or create branch. Not in a git repo"
+    fi
+}
+
 # Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
    export EDITOR='nvim'
@@ -30,4 +54,7 @@ alias icat="kitty +kitten icat"
 alias plint="pylama --linters=print,mccabe,pycodestyle,pyflakes --ignore=E501,W0612,W605,E231,E203"
 alias rgs="search_code"
 alias sshbac="TERM=xterm-256color ssh"  # include TERM in ssh for the *bac servers
+alias gcb="fzf-checkout"
 eval "$(mcfly init zsh)"
+
+
