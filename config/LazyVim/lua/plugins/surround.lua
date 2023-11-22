6d4fa02854726a84c768aeb402dc131171a839af
Michael Walsh <mwalsh2@atlassian.com>
2023-11-23 10:14:06 +1300

    Configure flash/surround so that I can continue the old surround keymaps

---
+++ config/LazyVim/lua/plugins/surround.lua
@@ -0,0 +1,10 @@
+return {
+  "echasnovski/mini.surround",
+  opts = {
+    mappings = {
+      add = "ys", -- Add surrounding in Normal and Visual modes
+      delete = "ds", -- Delete surrounding
+      replace = "cs", -- Replace surrounding
+    },
+  },
+}
