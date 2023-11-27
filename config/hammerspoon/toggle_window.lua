58d569851b6d79f9605753d06517410c03f2d0d8
Michael Walsh <mwalsh2@atlassian.com>
2023-11-27 15:11:39 +1300

    Actually get intellij launch/toggle working (different launcher name to active app name, ugh)

--- config/hammerspoon/toggle_window.lua
+++ config/hammerspoon/toggle_window.lua
@@ -1,20 +1,23 @@
-local toggle_window = function(opts, key, name, unit)
+local toggle_window = function(opts, key, name, unit, launcher_name)
 	if unit == nil then
 		unit = { x = 0.02, y = 0.02, w = 0.96, h = 0.96 }
+	end
+	if launcher_name == nil then
+		launcher_name = name
 	end
 	hs.hotkey.bind(opts, key, function()
 		local app = hs.application.get(name)
 		local screen = hs.screen.mainScreen()
 		if app then
 			if not app:mainWindow() then
-				app = hs.application.open(name, 2.0, true)
+				app = hs.application.open(launcher_name, 2.0, true)
 			elseif app:isFrontmost() then
 				app:hide()
 			else
 				app:activate()
 			end
 		else
-			app = hs.application.open(name, 2.0, true)
+			app = hs.application.open(launcher_name, 2.0, true)
 		end
 		app:mainWindow():moveToUnit(unit)
 	end)
@@ -26,6 +29,6 @@ toggle_window({ "ctrl", "alt" }, "s", "Spotify", { x = 0.1, y = 0.1, w = 0.8, h 
 toggle_window({ "ctrl" }, "s", "Slack", { x = 0.1, y = 0.1, w = 0.8, h = 0.8 })
 toggle_window({ "ctrl", "alt" }, "z", "zoom.us")
 toggle_window({ "ctrl", "alt" }, "tab", "Emacs", { x = 0.01, y = 0.01, w = 0.49, h = 0.99 })
-toggle_window({ "ctrl", "alt" }, "d", "IntelliJ IDEA Ultimate")
+toggle_window({ "ctrl", "alt" }, "d", "IntelliJ IDEA", nil, "IntelliJ IDEA Ultimate")
 toggle_window({ "ctrl", "alt" }, "c", "hammerspoon", { x = 0.2, y = 0.2, w = 0.5, h = 0.5 }) -- hammerspoon console
 toggle_window({ "ctrl", "alt" }, "k", "signal", { x = 0.1, y = 0.1, w = 0.8, h = 0.8 })
