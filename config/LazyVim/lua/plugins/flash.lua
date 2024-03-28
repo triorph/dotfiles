13fbb5aa0e0b849e5451b0ce7b04a13bd719ad1c
Michael Walsh <m.walsh@omni-nde.com>
2024-03-28 16:42:51 +1300

    Fix flash search to not escape if I type too much

--- config/LazyVim/lua/plugins/flash.lua
+++ config/LazyVim/lua/plugins/flash.lua
@@ -2,7 +2,11 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
-  opts = {},
+  opts = {
+    search = {
+      mode = "search",
+    },
+  },
   -- stylua: ignore
   keys = {
     { "x", mode = { "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
