c0f92fd22543681281d5c02a6069389f73d4b404
Michael Walsh <mwalsh2@atlassian.com>
2024-12-16 10:22:03 +1300

    Bug fixes to the virtual screen concept in hammerspoon

--- config/hammerspoon/virtual_screens.lua
+++ config/hammerspoon/virtual_screens.lua
@@ -1,6 +1,32 @@
 local M = {}
 
-local virtual_screen_multiplier = 1
+local virtual_screen_multiplier = { [0] = 1, [1] = 1 }
+local virtual_screen_id_start = function(real_screen_id)
+	local count = 0
+	for i = 0, real_screen_id do
+		if i < real_screen_id and virtual_screen_multiplier[i] ~= nil then
+			count = count + virtual_screen_multiplier[i]
+		end
+	end
+	return count
+end
+
+local deindex_virtual_screens = function(virtual_screen_id)
+	print("Trying to de-index virtual screen " .. virtual_screen_id)
+	local total_screens = #(hs.screen.allScreens())
+	local total_virtual_screens = virtual_screen_id_start(total_screens + 1)
+	for i = 0, total_virtual_screens do
+		if virtual_screen_id < virtual_screen_multiplier[i] then
+			print("Virtual screen de-indexed to screen " .. i .. " at virtual location " .. virtual_screen_id)
+			return i, virtual_screen_id
+		else
+			virtual_screen_id = virtual_screen_id - virtual_screen_multiplier[i]
+		end
+	end
+	print("Was unable to de-index virtual screen, so defaulting to 0,0")
+	return 0, 0
+end
+
 function M.get_current_virtual_screen(window)
 	hs.screen.allScreens()
 	local real_screen_id = nil
@@ -11,26 +37,38 @@ function M.get_current_virtual_screen(window)
 		end
 	end
 	if real_screen_id == nil then
-		print("Unable to find real screen for window. defaulting to 0")
-		real_screen_id = 0
+		print("Unable to find real screen for window. defaulting to 1")
+		real_screen_id = 1
 	end
+	local this_physical_screen = hs.screen.allScreens()[real_screen_id]
+	real_screen_id = real_screen_id - 1 -- zero-index, which we will undo when moving
 	-- TODO: make virtual_screens configurable and not limited to x-only directions
 	print(real_screen_id)
 	print(window)
-	print(window:topLeft())
+	print(window:frame())
+	print(this_physical_screen:absoluteToLocal(window:frame()))
 	if
-		virtual_screen_multiplier == 2
-		and window:topLeft().x > hs.screen.allScreens()[real_screen_id]:currentMode().w / 2
+		virtual_screen_multiplier[real_screen_id] == 2
+		and window:frame().x - this_physical_screen:frame().x > this_physical_screen:currentMode().w / 2
 	then
-		return real_screen_id * virtual_screen_multiplier + 1
+		return virtual_screen_id_start(real_screen_id) + 1
 	else
-		return real_screen_id * virtual_screen_multiplier
+		return virtual_screen_id_start(real_screen_id)
 	end
 end
 
 function M.get_next_virtual_screen(window)
 	local total_screens = #(hs.screen.allScreens())
-	return (M.get_current_virtual_screen(window) + 1) % (total_screens * virtual_screen_multiplier)
+	local total_virtual_screens = virtual_screen_id_start(total_screens + 1)
+	print(
+		"We have "
+			.. total_virtual_screens
+			.. " virtual screens and our current virtual screen is "
+			.. M.get_current_virtual_screen(window)
+	)
+	local ret = (M.get_current_virtual_screen(window) + 1) % total_virtual_screens
+	print("So we are moving to screen " .. ret)
+	return ret
 end
 
 function M.move_to_virtual_screen(window, virtual_screen, unit)
@@ -41,12 +79,13 @@ function M.move_to_virtual_screen(window, virtual_screen, unit)
 		virtual_screen = M.get_current_virtual_screen(window)
 	end
 	print("Moving to virtual screen " .. virtual_screen)
-	local real_screen = tonumber(virtual_screen / virtual_screen_multiplier)
-	local screens_virtual = virtual_screen % virtual_screen_multiplier
+	local real_screen, screens_virtual = deindex_virtual_screens(virtual_screen)
+	-- local real_screen = tonumber(virtual_screen / virtual_screen_multiplier)
+	-- local screens_virtual = virtual_screen % virtual_screen_multiplier
 	unit = {
-		x = unit.x / virtual_screen_multiplier + 0.5 * screens_virtual,
+		x = unit.x / virtual_screen_multiplier[real_screen] + 0.5 * screens_virtual,
 		y = unit.y,
-		w = unit.w / virtual_screen_multiplier,
+		w = unit.w / virtual_screen_multiplier[real_screen],
 		h = unit.h,
 	}
 	print(
@@ -57,7 +96,7 @@ function M.move_to_virtual_screen(window, virtual_screen, unit)
 			.. " giving us the location "
 	)
 	print(unit.x .. " " .. unit.y .. " " .. unit.w .. " " .. unit.h)
-	window:moveToScreen(hs.screen.allScreens()[real_screen])
+	window:moveToScreen(hs.screen.allScreens()[real_screen + 1]) -- remove zero index
 	window:moveToUnit(unit)
 end
 
