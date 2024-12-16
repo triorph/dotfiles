e8c94acf2c1cdc7257e997008afe42e429b747a1
Michael Walsh <mwalsh2@atlassian.com>
2024-12-17 10:13:51 +1300

    Tweak up zsh config to be a little tidier

--- config/zsh/miek-aliases-etc/miek-aliases-etc.zsh
+++ config/zsh/miek-aliases-etc/miek-aliases-etc.zsh
@@ -2,6 +2,25 @@
 export LESS="--quit-if-one-screen --RAW-CONTROL-CHARS --no-init"
 export FZF_DEFAULT_OPTS="--layout=reverse --height=10 -0"
 
+# double check everything we expect to be installed actually is 
+# (since its a bit manual these days)
+function health-check() {
+  type -p fd
+  type -p rg
+  type -p fzf
+  type -p bat
+  type -p lsd
+  type -p cargo
+  type -p nvim
+  type -p delta
+  type -p docker
+  type -p emacs
+  type -p jq
+  type -p zoxide
+  type -p tmux
+  type -p mise
+}
+
 # from https://github.com/beauwilliams/awesome-fzf/blob/master/awesome-fzf.zsh
 # Checkout to existing branch or else create new branch. gco <branch-name>.
 # Falls back to fuzzy branch selector list powered by fzf if no args.
@@ -26,6 +45,7 @@ fzf-checkout(){
     fi
 }
 
+# Lets you choose which nvim config to use with fzf for selection
 fzf-vim-config(){
     items=("default" "LazyVim")
     config=$(printf "%s\n" "${items[@]}"| fzf --prompt="Choose your neovim config" --height=~10 --exit-0 )
@@ -47,14 +67,17 @@ else
    export XIVIEWER='icat'
 fi
 
+# pipes rg into fzf and bat to let you browse your search
 function search_code {
    rg -n "$1" | fzf --delimiter=':' -n 1,3.. --preview-window 'right,70%,+{2}-/2' --preview 'bat --color=always {1} -H {2} --style=plain'
 }
 
+# exec into the bamboo build nodes (legacy)
 function kubeexec {
     kubectl exec -n "buildeng-$1-bamboo" --stdin --tty "$2" --container bamboo-agent  -- /bin/bash
 }
 
+# fzf to remove stale git branches from your local repository
 function gitbranchcleanup {
   git branch -vv \
     | grep ": gone" \
@@ -64,42 +87,61 @@ function gitbranchcleanup {
     | xargs git branch -D
 }
 
-ssh_all_prod() {                                          
-  for server in bbac ccbac depbac ecobac engbac idbac itbac jcbac jsbac mbac sabac sbac sgbac ssbac subac sdcbac soxbac tbac trebac; do
-    echo -n "$server: "
-    ssh $server "$*"
-  done
-}
-
+# Prefer kitty +kitten ssh, but be smart and only use it from kitty and from the host machine, and use
+# default ssh otherwise
 [ "$TERM" = "xterm-kitty" ] && [ "$SSH_CONNECTION" = "" ] && alias ssh="kitty +kitten ssh"
 
-# [[ ! -f ~/opt/homebrew/opt/asdf/libexec/asdf.sh ]] || source /opt/homebrew/opt/asdf/libexec/asdf.sh
-# [[ ! -f ~/.asdf/plugins/java/set-java-home.zsh ]] || source ~/.asdf/plugins/java/set-java-home.zsh
+# all in on LazyVim at this point. Should probably just move it over my main config.
 export NVIM_APPNAME="LazyVim"
+export VSCODE=code
 
+# git  aliases
 alias gca="git commit -v -a"
 alias gca!="git commit -v -a --amend --reset-author"
 alias gdh="git diff -r HEAD^"
 alias gdname="gd --cached --name-only --diff-filter=ACM -r"
-alias ls="lsd --group-dirs first"
-alias ll="lsd --group-dirs first --color=always -la | less"
-alias cat="bat --style plain --paging never"
-alias bs="bat --style plain"
+# ls aliases, only if lsd is installed
+! type lsd > /dev/null || alias ls="lsd --group-dirs first"
+! type lsd > /dev/null || alias ll="lsd --group-dirs first --color=always -la | less"
+# cat aliases, only if bat is installed
+! type bat > /dev/null || alias cat="bat --style plain --paging never"
+! type bat > /dev/null || alias bs="bat --style plain"
+# diff alias, only if delta is installed
+! type delta > /dev/null || alias diff="delta"
+
+# fzf aliases, only if fzf is installed
+! type fzf > /dev/null || alias gcb="fzf-checkout"
+! type fzf > /dev/null || alias nvims="fzf-vim-config"
+! type fzf > /dev/null || alias vims="fzf-vim-config"
+! type fzf > /dev/null || alias rgs="search_code"
+# editor aliases
 alias emacs="emacs -nw"
-alias xgrep="fd -tf .py . | xargs grep --color=always --exclude-dir={.git,.svn.CVS} "
-alias xgrepfull="fd -tf .py . | xargs grep --color=always -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} "
 alias edit="$EDITOR"
-alias diff="delta"
+alias vim="nvim"
+alias health_check="health-check"
+
+# allow image viewing in the kitty terminal
 alias icat="kitty +kitten icat"
-alias plint="pylama --linters=print,mccabe,pycodestyle,pyflakes --ignore=E501,W0612,W605,E231,E203"
-alias rgs="search_code"
-alias sshbac="TERM=xterm-256color ssh"  # include TERM in ssh for the *bac servers
-alias gcb="fzf-checkout"
-alias nvims="fzf-vim-config"
-alias vims="fzf-vim-config"
+
+# docker aliases
 alias dbash="docker run --entrypoint /bin/bash -it --pull always"
 alias dsh="docker run --entrypoint /bin/sh -it --pull always"
-alias vim="nvim"
+
+# edit your shell in an editor if the line is too complicated
 bindkey '^x^e' edit-command-line
+
+# I think this is supposed to be ~/.zprofile and I got confused at some point, but 
+# doesn't hurt to keep (especially with the check)
+[[ ! -f ~/.zsh_profile ]] || source ~/.zsh_profile  
+
+# local fzf changes, if they exist
+[[ ! -f ~/.fzf.zsh ]] || (source ~/.fzf.zsh && bindkey "ç" fzf-cd-widget)
+
+# Activate mise, accounting for the 2 ways to install it
+[[ ! -f /opt/homebrew/bin/mise ]] || eval "$(/opt/homebrew/bin/mise activate zsh)"
+[[ ! -f $HOME/.local/bin/mise ]] || eval "$($HOME/.local/bin/mise activate zsh)"
+
+# atlas kitt context setup, only if atlas cli exists
+export KUBECONFIG=$([[ ! -f /opt/atlassian/bin/atlas ]] || /opt/atlassian/bin/atlas kitt context:create --pid=$$)
 
 
