0a8a4e8c83baf9e8b5b8c3338eaf00a2e74fe69d
Michael Walsh <m.walsh@omni-nde.com>
2025-01-13 19:12:20 +1300

    update hammerspoon with per system toggles

--- config/hammerspoon/toggle_window.lua
+++ config/hammerspoon/toggle_window.lua
@@ -28,24 +28,22 @@ local toggle_window = function(opts, key, name, unit, launcher_name)
 	end)
 end
 
-local web_browser = "Firefox" -- default to firefox
 print(hs.host.localizedName())
 if hs.host.localizedName() == "CJDPHHJW5Q" then -- work laptop uses Chrome
-	web_browser = "Chrome"
+	toggle_window({ "ctrl" }, "tab", "chrome")
+	toggle_window({ "ctrl", "alt" }, "d", "IntelliJ IDEA", nil, "IntelliJ IDEA Ultimate")
 elseif hs.host.localizedName() == "Michael’s MacBook Pro" then -- home laptop with firefox
-	web_browser = "Firefox"
+	toggle_window({ "ctrl" }, "tab", "Firefox")
+	-- so glad to be rid of teams
+	-- toggle_window({ "ctrl", "alt" }, "z", "Microsoft Teams (work or school)", { x = 0.1, y = 0.1, w = 0.8, h = 0.8 })
+	toggle_window({ "ctrl", "alt" }, "d", "Parsec", nil, "Parsec")
+	toggle_window({ "ctrl", "alt" }, "k", "Messages", { x = 0.1, y = 0.1, w = 0.8, h = 0.8 })
+	toggle_window({ "ctrl" }, "m", "Mail", { x = 0.1, y = 0.1, w = 0.8, h = 0.8 })
 end
 
 toggle_window({ "ctrl" }, "`", "kitty")
-toggle_window({ "ctrl" }, "tab", web_browser)
 toggle_window({ "ctrl", "alt" }, "s", "Spotify", { x = 0.1, y = 0.1, w = 0.8, h = 0.8 })
 toggle_window({ "ctrl" }, "s", "Slack", { x = 0.1, y = 0.1, w = 0.8, h = 0.8 })
--- so glad to be rid of teams
--- toggle_window({ "ctrl", "alt" }, "z", "Microsoft Teams (work or school)", { x = 0.1, y = 0.1, w = 0.8, h = 0.8 })
 toggle_window({ "ctrl", "alt" }, "z", "zoom.us")
 toggle_window({ "ctrl", "alt" }, "tab", "Emacs", { x = 0.01, y = 0.01, w = 0.49, h = 0.99 })
-toggle_window({ "ctrl", "alt" }, "d", "IntelliJ IDEA", nil, "IntelliJ IDEA Ultimate")
--- toggle_window({ "ctrl", "alt" }, "d", "Parsec", nil, "Parsec")
 toggle_window({ "ctrl", "alt" }, "c", "hammerspoon", { x = 0.2, y = 0.2, w = 0.5, h = 0.5 }) -- hammerspoon console
-toggle_window({ "ctrl", "alt" }, "k", "Messages", { x = 0.1, y = 0.1, w = 0.8, h = 0.8 })
-toggle_window({ "ctrl" }, "m", "Mail", { x = 0.1, y = 0.1, w = 0.8, h = 0.8 })
