07908cfa2b267f755827b80fd9971bd2e21b764a
Michael Walsh <triorph@gmail.com>
2026-04-11 09:39:05 +1200

    Turn off some toggle_window modes

--- config/hammerspoon/toggle_window.lua
+++ config/hammerspoon/toggle_window.lua
@@ -42,12 +42,12 @@ print(hs.host.localizedName())
 if hs.host.localizedName() == "CJDPHHJW5Q" then -- work laptop
 	toggle_window({ "ctrl", "alt" }, "d", "IntelliJ IDEA", nil, "IntelliJ IDEA Ultimate")
 	toggle_window({ "ctrl", "alt" }, "g", "Goland", nil, "Goland")
-	toggle_window({ "ctrl", "alt", "cmd" }, "d", "Cursor")
+	-- toggle_window({ "ctrl", "alt", "cmd" }, "d", "Cursor")
 	toggle_window({ "ctrl" }, "tab", "Arc")
 elseif hs.host.localizedName() == "Michael’s MacBook Pro" then -- home laptop
 	-- so glad to be rid of teams
 	-- toggle_window({ "ctrl", "alt" }, "z", "Microsoft Teams (work or school)", { x = 0.1, y = 0.1, w = 0.8, h = 0.8 })
-	toggle_window({ "ctrl", "alt" }, "d", "Parsec", nil, "Parsec")
+	-- toggle_window({ "ctrl", "alt" }, "d", "Parsec", nil, "Parsec")
 	toggle_window({ "ctrl", "alt" }, "k", "Messages", { x = 0.1, y = 0.1, w = 0.8, h = 0.8 })
 	toggle_window({ "ctrl" }, "tab", "Firefox")
 end
